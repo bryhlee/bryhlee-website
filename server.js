0a21bfc51defd1ec8e22f9e3fc3547dca1674610
bryhlee <bhlee@scu.edu>
2016-03-15 20:04:47 -0700

    Performance increase server.js

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-/* -------- DEPENDENCIES -------- */
+/* -------- LOAD EXPRESS-------- */
 var express = require('express');
 var server = express();
 
-var bodyParser = require('body-parser');
-var path = require('path');
-
-
-server.use(bodyParser.urlencoded( {extended: true }));
-server.use(bodyParser.json() );
-
-/* -------- ROUTES -------- */
-server.use(express.static(__dirname + '/main'));
-server.use(function(req, res) {
+/* -------- SERVE FILES -------- */
+server.use(express.static(__dirname + '/'));
+server.use(function(sreq, res) {
     res.sendFile(__dirname + '/main/index.html');
 });
 
+/* -------- DEPENDENCIES -------- */
+var bodyParser = require('body-parser');
+var path = require('path');
+
+server.use(bodyParser.urlencoded({extended: true }));
+server.use(bodyParser.json() );
+
+
+/* -------- RUN LOCAL SERVER -------- */
 server.listen(process.env.PORT || 8080, function () {
     console.log("Magic happens at ", process.env.PORT || 8080);
 });
+
 
