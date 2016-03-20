fa3cde1a7d0776fc7f36e6166dfc2653b48463d5
bryhlee <bhlee@scu.edu>
2016-03-20 15:08:55 -0700

    Added sticky sidebar

--- main/scripts/app.js
+++ main/scripts/app.js
@@ -1,18 +1,21 @@
-(function() {
-    
-    var app = angular.module('bryhlee-website', ['ngRoute']);
 
-    app.config(['$routeProvider', '$locationProvider',
-      function($routeProvider, $locationProvider) {
-        $routeProvider
-          .when('/', {
-            templateUrl: 'views/home.html'
-          })
-          .otherwise({
-            redirectTo: '/' 
-          });
-        $locationProvider.html5Mode(true);
-      }]
-    );
+var app = angular.module('bryhlee-website', ['ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html'
+      })
+      .when('/about', {
+        templateUrl: 'views/home.html'
+      })
+    $locationProvider.html5Mode(true);
+  }]
+);
+
+app.controller('dialogController', function($scope) {
+    $('.sticky').Stickyfill();
   
-}());
+    
+});
