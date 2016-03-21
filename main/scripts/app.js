4f6954a37249838a91b386f5bf65c7435f4914e3
bryhlee <bhlee@scu.edu>
2016-03-20 18:21:03 -0700

    Added smoothscroll

--- main/scripts/app.js
+++ main/scripts/app.js
@@ -15,7 +15,22 @@ app.config(['$routeProvider', '$locationProvider',
 );
 
 app.controller('dialogController', function($scope) {
-    $('.sticky').Stickyfill();
+  // Position: sticky polyfill
+  $('.sticky').Stickyfill();
   
+  // Smoothscroll on dialog links
+  $('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 300, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
     
 });
+
